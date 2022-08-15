c0811665e53e4ce7cc914ebda6eb9651d37a2591
Bangaly KABA <b.kaba@groupeonepoint.com>
2022-08-15 12:07:24 +0200

    clen and update

--- src/main/java/tresorkey/MonkeyIslandApiExample.java
+++ src/main/java/tresorkey/MonkeyIslandApiExample.java
@@ -1,11 +1,6 @@
 package tresorkey;
 
-
-import java.io.File;
 import java.io.IOException;
-import java.util.*;
-
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -22,43 +17,37 @@ import okhttp3.Response;
 @SpringBootApplication
 public class MonkeyIslandApiExample {
 
-    public static void main(String[] args) {
-    	
-    	SpringApplication.run(MonkeyIslandApiExample.class, args);
-    	
-    	Logger log = LoggerFactory.getLogger(MonkeyIslandApiExample.class);
-    	
-    	Response response = null;
-    	OkHttpClient client = new OkHttpClient().newBuilder().build();
-    			MediaType mediaType = MediaType.parse("text/plain");
-    			RequestBody body = RequestBody.create(mediaType, "");
-    			Request request = new Request.Builder()
-    			  .url("https://gfms-sandbox-monkeyisland.azurewebsites.net/key0729")
-    			  .method("GET", null)
-    			  .addHeader("Accept", "application/json")
-    			  .build();
-    			try {
-					response = client.newCall(request).execute();
-					String res = response.body().string();
-					log.info("Resultat : " + res);
-					JSONObject jobject = new JSONObject(res); 
-					JSONArray jArray = jobject.getJSONArray("magicNumbers");
-					log.info("jArray : " + jArray);
-					int sumMagicNumbers =0;
-					 for (int i = 0; i < jArray.length(); i++) {
-						 log.info("jArray : " + jArray.getInt(i));
-						 sumMagicNumbers += jArray.getInt(i);
-					 }
-					 
-					 log.info("sumMagicNumbers : " + sumMagicNumbers);
-					
-				} catch (IOException e) {
-					
-					log.error(e.getMessage());
-					// TODO Auto-generated catch block
-					//e.printStackTrace();
-				} 	
-    	
-    
-    }
+	public static void main(String[] args) {
+
+		SpringApplication.run(MonkeyIslandApiExample.class, args);
+
+		Logger log = LoggerFactory.getLogger(MonkeyIslandApiExample.class);
+
+		Response response = null;
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		MediaType mediaType = MediaType.parse("text/plain");
+		RequestBody body = RequestBody.create(mediaType, "");
+		Request request = new Request.Builder().url("https://gfms-sandbox-monkeyisland.azurewebsites.net/key0729")
+				.method("GET", null).addHeader("Accept", "application/json").build();
+		try {
+			response = client.newCall(request).execute();
+			String res = response.body().string();
+			log.info("Resultat : " + res);
+			JSONObject jobject = new JSONObject(res);
+			JSONArray jArray = jobject.getJSONArray("magicNumbers");
+			log.info("jArray : " + jArray);
+			int sumMagicNumbers = 0;
+			for (int i = 0; i < jArray.length(); i++) {
+				log.info("jArray : " + jArray.getInt(i));
+				sumMagicNumbers += jArray.getInt(i);
+			}
+
+			log.info("sumMagicNumbers : " + sumMagicNumbers);
+
+		} catch (IOException e) {
+
+			log.error(e.getMessage());
+		}
+
+	}
 }
